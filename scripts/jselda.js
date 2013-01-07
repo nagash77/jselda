112727b81cdbb8cdd4a05e1b6fec42f8afdf8b0b
unknown <bmaher@CHIWBMAHER.onpeak.us>
2013-01-07 12:09:47 -0600

    little more basic shit added in

---
+++ scripts/jselda.js
@@ -0,0 +1,29 @@
+
+
+jselda = {};
+
+jselda.main = function(canvas) {
+
+	var mainCanvas = canvas;
+	var context = mainCanvas.getContext('2d');
+
+	function main () {
+	  handleInput();
+	  update();
+	  render();
+	};
+
+	function handleInput() {
+
+	}
+
+	function update() {
+
+	}
+
+	function render() {
+		
+	}
+
+	setInterval(main, 1);
+};
