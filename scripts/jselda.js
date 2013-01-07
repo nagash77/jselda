5b07d8cfe6fa89556382479d78b984166e256a98
unknown <bmaher@CHIWBMAHER.onpeak.us>
2013-01-07 15:21:19 -0600

    getting framework setup a bit

--- scripts/jselda.js
+++ scripts/jselda.js
@@ -1,36 +1,53 @@
 
+// Create the canvas
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 800;
+document.body.appendChild(canvas);
 
-jselda = {};
 
-jselda.main = function(canvas) {
-
-	var mainCanvas = canvas;
-	var context = mainCanvas.getContext('2d');
-
-	function main () {
-	  var now = Date.now();
-	  var delta = now - then;
-
-	  handleInput();
-	  update(delta / 1000);
-	  render();
-
-	  then = now;
-	};
-
-	function handleInput() {
-
-	}
-
-	function update() {
-
-	}
-
-	function render() {
-
-	}
-
-	var then = Date.now();
-	setInterval(main, 1);
-
+var heroObj = {
+	speed : 256 // movement in pixels per second
 };
+
+
+// Handle keyboard controls
+var keysDown = {};
+
+addEventListener("keydown", function (e) {
+	keysDown[e.keyCode] = true;
+}, false);
+
+addEventListener("keyup", function (e) {
+	delete keysDown[e.keyCode];
+}, false);
+
+
+function handleInput() {
+
+}
+
+function update() {
+
+}
+
+function render() {
+
+}
+
+function main () {
+	var now = Date.now();
+	var delta = now - then;
+
+	handleInput();
+	update(delta / 1000);
+	render();
+
+	then = now;
+};
+
+var then = Date.now();
+setInterval(main, 1);
+
+
