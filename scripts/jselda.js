72133bc03f9f1e031e58af9684d61ca39a969bb6
unknown <bmaher@CHIWBMAHER.onpeak.us>
2013-01-09 14:10:45 -0600

    got a VERY basic hero object moving around on the screen

--- scripts/jselda.js
+++ scripts/jselda.js
@@ -1,4 +1,3 @@
-
 // Create the canvas
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
@@ -6,14 +5,23 @@ canvas.width = 800;
 canvas.height = 800;
 $('body').append(canvas);
 
-ctx.fillStyle = "rgba(0, 0, 255, .5)";
-ctx.fillRect(25, 25, 125, 125);
+ctx.fillStyle = "rgb(42,161,87)";
+ctx.fillRect (0, 0, 800, 800);
 
+//setup our hero object
 var heroObj = {
-	speed : 256 // movement in pixels per second
+	speed : 175, // movement in pixels per second
+	x     : canvas.width / 2, //x coordinate position
+	y     : canvas.height / 2 //y coordinate position
 };
 
-
+var heroReady = false;
+var heroImageObj = new Image();
+heroImageObj.onload = (function() {	
+	heroImageObj.src = 'img/holderLink.png';
+	heroObj.imageObj = heroImageObj;
+	heroReady = true;
+})();
 // Handle keyboard controls
 var keysDown = {};
 
@@ -25,20 +33,35 @@ addEventListener("keyup", function (e) {
 	delete keysDown[e.keyCode];
 }, false);
 
+var handleInput = function() {
 
-function handleInput() {
+};
 
-}
+var update = function(modifier) {
+	if (38 in keysDown) { // Player holding up
+		heroObj.y -= heroObj.speed * modifier;
+	}
+	if (40 in keysDown) { // Player holding down
+		heroObj.y += heroObj.speed * modifier;
+	}
+	if (37 in keysDown) { // Player holding left
+		heroObj.x -= heroObj.speed * modifier;
+	}
+	if (39 in keysDown) { // Player holding right
+		heroObj.x += heroObj.speed * modifier;
+	}
+};
 
-function update() {
+var render = function() {
+	ctx.fillStyle = "rgb(42,161,87)";
+	ctx.fillRect (0, 0, 800, 800);
 
-}
+	if(heroReady) {
+		ctx.drawImage(heroImageObj, heroObj.x, heroObj.y, 50, 80);
+	}
+};
 
-function render() {
-
-}
-
-function main () {
+var main = function() {
 	var now = Date.now();
 	var delta = now - then;
 
@@ -51,5 +74,3 @@ function main () {
 
 var then = Date.now();
 setInterval(main, 1);
-
-
